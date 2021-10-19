cc4ced74e218ee9b73b9890354ca89ea22ee0b2f
Chase Kessler <chasekessler30@yahoo.com>
2021-10-19 16:55:21 -0400

    booking routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.spots_routes import spots_routes
+from .api.bookings_routes import booking_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(spots_routes, url_prefix="/api/spots")
+app.register_blueprint(booking_routes, url_prefix="/api/bookings")
 db.init_app(app)
 Migrate(app, db)
 
