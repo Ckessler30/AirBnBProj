486909dcfb508ef606647fcd8cc36e87b2222a7f
Chase Kessler <chasekessler30@yahoo.com>
2021-10-20 14:28:54 -0400

    added format

--- react-app/src/components/CheckIn/CheckIn.js
+++ react-app/src/components/CheckIn/CheckIn.js
@@ -9,36 +9,36 @@ import { addBooking } from '../../store/bookings'
 function CheckIn({ spot }) {
     const dispatch = useDispatch()
     const {user} = useSelector(state => state.session)
-    // console.log(user)
+    const bookings = useSelector(state => state.bookings)
+    const userBooks = bookings.filter(booking => booking.userId === user.id && booking.spotId === spot.id)
+    const spotBookings = bookings.filter(booking => booking.spotId === spot.id)
 
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
     const [numGuests, setNumGuests] = useState(1)
     const [openCalendar, setOpenCalendar] = useState(false)
     const [openGuests, setOpenGuests] = useState(false)
+    const [isBooked, setIsBooked] = useState(userBooks.length ? true : false)
     const [errors, setErrors] = useState([])
-    // const date1 = new Date(spot.bookings[0].startDate.split(',').join(''))
-    // const date2 = new Date(spot.bookings[0].endDate.split(',').join(''))
+
+    const currBookedDates = bookedDates(spotBookings)
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
         key: 'selection'
     }
-    const currBookedDates = bookedDates(spot.bookings)
-    console.log(currBookedDates)
-    console.log(new Date(startDate.toString().split(' ').slice(0,4).join(' ')))
+
     const handlePicker = (ranges) => {
-        // console.log(ranges.selection)
         setStartDate(ranges.selection.startDate)
         setEndDate(ranges.selection.endDate)
     }
+
     const clearDates = () => {
         setStartDate(new Date())
         setEndDate(new Date())
     }
-    // console.log("Start Date", startDate)
-    // console.log("END DATE", endDate)
-
+    console.log(currBookedDates)
+    console.log(startDate)
     const handleReserve = () => {
         let booked = false
         for(let i = 0; i < currBookedDates.length; i++){
@@ -58,7 +58,6 @@ function CheckIn({ spot }) {
         }
         if(booked){
             const newErrors = ["That date is unavailable"]
-            // console.log("BOOKED")
             setErrors(newErrors)
         }else{
             const newBooking = {
@@ -75,8 +74,10 @@ function CheckIn({ spot }) {
           console.log("Done")
         }
     }
-    // console.log(errors)
-
+    
+    const split = userBooks[0].startDate.split(" ");
+    const formatResDate = `${split[2]} ${split[1]}, ${split[3]}`
+    
   return (
     <div className="check-in-container">
       <div>
@@ -139,10 +140,19 @@ function CheckIn({ spot }) {
       )}
       <div>
           <button onClick={handleReserve} disabled={startDate === endDate}>Reserve</button>
-          {errors.length && 
+          {errors.length ? 
             <p>{errors}</p>
+            :
+            null
           }
       </div>
+      {userBooks.length ?
+        <div>
+            <p>You are all set for your reservation on {formatResDate}</p>
+        </div>
+        :
+        null
+      }
     </div>
   );
 }
