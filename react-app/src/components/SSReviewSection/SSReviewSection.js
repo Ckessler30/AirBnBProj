4046e34cf8d59791b8d4f43b1876f558832f4eaa
Chase Kessler <chasekessler30@yahoo.com>
2021-10-22 19:01:16 -0400

    added some stuff to progress bar

--- react-app/src/components/SSReviewSection/SSReviewSection.js
+++ react-app/src/components/SSReviewSection/SSReviewSection.js
@@ -7,10 +7,12 @@ import './SSReview.css'
 function SSReviewSection({spotReviews, revSec}) {
     // console.log("REVIEW",spot)
     const avg = specRevAvg(spotReviews, revSec)
+    console.log("avg",avg)
+    console.log("spot rev", spotReviews)
     return (
         <div className="progress-bar-wrapper">
-            <ProgressBar now={avg} max={5}/> 
-            {spotReviews.length > 0 && <p className="prog-num">{avg}</p>}
+            <ProgressBar now={spotReviews.length > 0  ? avg : 0} max={5}/> 
+            {spotReviews.length > 0 ? <p className="prog-num">{avg}</p> : <p className="prog-num">0</p>}
         </div>
     )
 }
