96c5c4abcdc8421cfd3ba64aaef95752cfd4368f
Chase Kessler <chasekessler30@yahoo.com>
2021-10-25 13:11:36 -0400

    added styling to cs page

--- react-app/src/components/CreateSpot/CreateSpot.js
+++ react-app/src/components/CreateSpot/CreateSpot.js
@@ -53,6 +53,7 @@ function CreateSpot() {
             setErrors(data.errors)
         }
         if(data.name && !data.errors ){
+            console.log("MADE IT IN")
             const newPic = await dispatch(addSpotPic({spotId:data.id, imgUrl: pic1}))
             if(pic2){
                 await dispatch(addSpotPic({spotId:data.id, imgUrl:pic2}))
@@ -67,80 +68,173 @@ function CreateSpot() {
     console.log(errors)
 
     return (
-        <div className="cs-container">
-            <form onSubmit={handleSubmit}>
-                <div className="cs-header">
-                    <p>Host a spot</p>
-                </div>
-                <div className="err-box">
-                {errors.length > 0 && errors.map(error=>
-
-                    <p className="login-err">{error}</p>
-                    )}
-                    </div>
-                <div className="cs-input-field">
-                    <h3>Spot Name</h3>
-                    <input type="text" placeholder="What is the name of your getaway?" onChange={(e)=>setSpotName(e.target.value)} value={spotName} required/>
-                </div>
-                <div className="cs-input-field">
-                    <h3>Price per night</h3>
-                    <input type="number" placeholder={"$"+ 0} onChange={(e)=>setSpotPrice(e.target.value)} value={spotPrice} required/>
-                </div>
-                <div className="cs-input-field">
-                    <h3>Please give a description of your getaway</h3>
-                    <textarea name="" id="" cols="30" rows="5" onChange={(e)=>setDescription(e.target.value)} value={description} required placeholder="Tell us about your getaway"></textarea>
-                </div>
-                <div className="cs-input-field">
-                    <p>What kind of place is this?</p>
-                    <select name="type" id="" onChange={(e)=>setType(e.target.value)} value={type} required>
-                        {/* <option value="">Please select an option</option> */}
-                        <option value="Condo">Condo</option>
-                        <option value="House">House</option>
-                        <option value="Villa">Villa</option>
-                        <option value="Cabin">Cabin</option>
-                        <option value="Tree House">Tree House</option>
-                        <option value="Beach House">Beach House</option>
-                        <option value="Camp Site">Camp Site</option>
-                    </select>
-                </div>
-                <div className="cs-input-field">
-                    <p>How many bedrooms?</p>
-                    <input type="number" onChange={(e)=> setNumBedrooms(e.target.value)} placeholder="0 bedrooms" value={numBedrooms} required/>
-                </div>
-                <div className="cs-input-field">
-                    <p>How many bathrooms?</p>
-                    <input type="number" onChange={(e)=>setNumBaths(e.target.value)} value={numBaths} placeholder="0 baths" required/>
-                </div>
-                <div className="cs-input-field">
-                    <p>How many beds?</p>
-                    <input type="number" onChange={(e)=> setNumBeds(e.target.value)} value={numBeds} placeholder="0 beds" required/>
-                </div>
-                <div className="cs-input-field">
-                    <p>What is the max occupancy?</p>
-                    <input type="number" onChange={(e)=> setTotalGuests(e.target.value)} value={totalGuests} placeholder="0 guests" required/>
-                </div>
-                <div className="cs-input-field">
-                    <h3>Location</h3>
-                    <select name="state" placeholder="State" onChange={(e)=>setState(e.target.value)} value={state} required>
-                        {stateList && stateList.map(state => (
-                            <option value={state}>{state}</option>
-                        ))}
-                        </select>
-                    <input type="text" placeholder="Street Address" onChange={(e)=>setAddress(e.target.value)} value={address} required/>
-                    <input type="number" placeholder="Longitude" onChange={(e)=>setLong(e.target.value)} value={long} required/>
-                    <input type="number" placeholder="Latitude" onChange={(e)=> setLat(e.target.value)} value={lat} required/>
-                </div>
-                <div className="cs-input-field">
-                    <h3>Pictures</h3>
-                    <input type="text" placeholder="Picture Url" onChange={(e)=> setPic1(e.target.value)} value={pic1} required/>
-                    <input type="text" placeholder="Picture Url" onChange={(e)=> setPic2(e.target.value)} value={pic2}/>
-                    <input type="text" placeholder="Picture Url" onChange={(e)=> setPic3(e.target.value)} value={pic3}/>
-                </div>
-                <button type="submit">Submit</button>
-                
-            </form>
-        </div>
-    )
+      <div className="cs-container">
+        <form onSubmit={handleSubmit}>
+          <div className="cs-header">
+            <p>Host a spot</p>
+          </div>
+          <div className="err-box">
+            {errors.length > 0 &&
+              errors.map((error) => <p className="login-err">{error}</p>)}
+          </div>
+          <div className="cs-input-field">
+            <h3>Spot Name</h3>
+            <input
+              type="text"
+              placeholder="What is the name of your getaway?"
+              onChange={(e) => setSpotName(e.target.value)}
+              value={spotName}
+              required
+            />
+          </div>
+          <div className="cs-input-field">
+            <h3>Price per night</h3>
+            <input
+              type="number"
+              placeholder={"$" + 0}
+              onChange={(e) => setSpotPrice(e.target.value)}
+              value={spotPrice}
+              required
+            />
+          </div>
+          <div className="cs-input-field">
+            <h3>Please give a description of your getaway</h3>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+              placeholder="Tell us about your getaway"
+            ></textarea>
+          </div>
+          <div className="cs-input-field">
+            <p>What kind of place is this?</p>
+            <select
+              name="type"
+              id=""
+              onChange={(e) => setType(e.target.value)}
+              value={type}
+              required
+            >
+              {/* <option value="">Please select an option</option> */}
+              <option value="Condo">Condo</option>
+              <option value="House">House</option>
+              <option value="Villa">Villa</option>
+              <option value="Cabin">Cabin</option>
+              <option value="Tree House">Tree House</option>
+              <option value="Beach House">Beach House</option>
+              <option value="Camp Site">Camp Site</option>
+            </select>
+          </div>
+          <div className="cs-input-field">
+            <p>How many bedrooms?</p>
+            <input
+              type="number"
+              onChange={(e) => setNumBedrooms(e.target.value)}
+              placeholder="0 bedrooms"
+              value={numBedrooms}
+              required
+            />
+          </div>
+          <div className="cs-input-field">
+            <p>How many bathrooms?</p>
+            <input
+              type="number"
+              onChange={(e) => setNumBaths(e.target.value)}
+              value={numBaths}
+              placeholder="0 baths"
+              required
+            />
+          </div>
+          <div className="cs-input-field">
+            <p>How many beds?</p>
+            <input
+              type="number"
+              onChange={(e) => setNumBeds(e.target.value)}
+              value={numBeds}
+              placeholder="0 beds"
+              required
+            />
+          </div>
+          <div className="cs-input-field">
+            <p>What is the max occupancy?</p>
+            <input
+              type="number"
+              onChange={(e) => setTotalGuests(e.target.value)}
+              value={totalGuests}
+              placeholder="0 guests"
+              required
+            />
+          </div>
+          <div className="cs-input-field loc">
+            <h3>Location</h3>
+            <select
+              name="state"
+              placeholder="State"
+              onChange={(e) => setState(e.target.value)}
+              value={state}
+              required
+            >
+              {stateList &&
+                stateList.map((state) => (
+                  <option value={state}>{state}</option>
+                ))}
+            </select>
+            <h3>Street Address</h3>
+            <input
+              type="text"
+              placeholder="Street Address"
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+              placeholder="...123 Example Rd, Example City, VA 22201"
+              required
+            />
+            <h3>Longitude</h3>
+            <input
+              type="number"
+              placeholder="Longitude"
+              onChange={(e) => setLong(e.target.value)}
+              value={long}
+              required
+            />
+            <h3>Latitude</h3>
+            <input
+              type="number"
+              placeholder="Latitude"
+              onChange={(e) => setLat(e.target.value)}
+              value={lat}
+              required
+            />
+          </div>
+          <div className="cs-input-field pics">
+            <h3>Pictures</h3>
+            <input
+              type="text"
+              placeholder="Picture Url"
+              onChange={(e) => setPic1(e.target.value)}
+              value={pic1}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Picture Url"
+              onChange={(e) => setPic2(e.target.value)}
+              value={pic2}
+            />
+            <input
+              type="text"
+              placeholder="Picture Url"
+              onChange={(e) => setPic3(e.target.value)}
+              value={pic3}
+            />
+          </div>
+          <button className="reserve-btn" type="submit">Submit</button>
+        </form>
+      </div>
+    );
 }
 
 export default CreateSpot
