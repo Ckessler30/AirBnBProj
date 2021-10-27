b5bedecf2146d190d2e75d7ec93e3172fd2c7197
Chase Kessler <chasekessler30@yahoo.com>
2021-10-27 17:28:13 -0400

    fixed password error

--- react-app/src/components/utils.js
+++ react-app/src/components/utils.js
@@ -71,6 +71,9 @@ export const errorHandler = (err) => {
     if(splitErr[0] === "password " && splitErr[1] === " This field is required."){
         return "Please provide a password."
     }
+    if(splitErr[0] === "password " && splitErr[1] === " Password was incorrect."){
+        return "Password was incorrect."
+    }
     if(splitErr[0] === "email " && splitErr[1] === " This field is required."){
         return "Please provide an email."
     }
